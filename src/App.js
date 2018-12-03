68218b7340b045bbea7d3f55ee9946d3ed96f7c8
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-12-02 23:50:26 -0500

    modified render

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     fairPics: null
   }
 
+//fetches image data from job site manager back end
   getFairPics = () => {
     const baseUrl = `http://localhost:3000/api/v1`;
     return fetch(`${baseUrl}/images`, {
@@ -38,9 +39,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         { this.state.fairPics && this.renderFairImages() }
       </div>
     );
